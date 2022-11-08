a0009f6019136c4056406bea9a92aec3108313a9
Nycolas Kevin <nycolas.costa@dcx.ufpb.br>
2022-11-08 10:54:41 -0300

    views modified

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+'''
+Aqui temos que criar algumas rotas
+'''
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="home"),
+    path("user/", views.userPage, name="userPage")
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("hello world!")
+
+def userPage(request):
+    return render(request, "home.html")
