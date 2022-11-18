6403d51b7694a859eeac7247082f972300103b82
Nycolas Kevin <nycolas.kevin2@gmail.com>
2022-11-18 11:21:24 -0300

    models update

--- homePage/models.py
+++ homePage/models.py
@@ -2,6 +2,26 @@ from django.db import models
 
 # Create your models here.
 
-class Manga(models):
-    name = models.CharField()
-    frontCover = models.ImageField(upload_to="mangaFrontCover")
+class Manga(models.Model):
+    manga_id = models.BigAutoField(primary_key=True)
+    manga_name = models.CharField(max_length=25)
+    frontc_over = models.ImageField(upload_to="mangasFrontCover")
+
+    def __str__(self) -> str:
+        return self.manga_name
+
+    @property
+    def manga_id(self):
+        return self.manga_id
+
+class User(models.Model):
+    user_id = models.BigAutoField(primary_key=True)
+    user_name = models.CharField(max_length=12)
+    fav_mangas = models.ManyToManyField(Manga)
+
+    def __str__(self) -> str:
+        return self.user_name
+
+    @property
+    def user_id(self):
+        return self.user_id
